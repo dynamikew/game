1a8cba171ec67cc945e3f6dd8f3964ff4f83aae4
michael james waldron <dynamikew@michaels-MacBook-Pro.local>
2017-08-02 18:06:05 -0400

    added open truth and dare pages functions

---
+++ truth or dare code/tOrD.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$( ".b1" ).click(function() {
+ 	 window.open( "truth.html" );
+	}); 	// end of b1 click function
+
+	
+	$( ".b2" ).click(function() {
+	  window.open( "dare.html" );
+	}); 	// end of b2 click function
+
+});	// end of Document Ready
